6ef5c1ae3756fcb03f5673b6df76559d234ebee3
matthewp03 <150979906+matthewp03@users.noreply.github.com>
2023-11-15 20:58:52 -0500

    Update CNIT37200_Milestone3.sql

---
+++ CNIT37200_Milestone3.sql
@@ -0,0 +1,11 @@
+/* The project spec says we need a function or procedure for all of the questions, 
+a cursor for at least 1 of the questions, a trigger for at least 1 question, and a package
+for all of the functions and procedures. */
+create or replace package project_package as
+
+  
+end project_package;
+
+create or replace package body project_package as
+
+end project_package;
