2816ec808282343675c24434d0aa0f3d936e4480
matthewp03 <150979906+matthewp03@users.noreply.github.com>
2023-11-29 16:43:32 -0500

    Update CNIT37200_Milestone3.sql

--- CNIT37200_Milestone3.sql
+++ CNIT37200_Milestone3.sql
@@ -1,6 +1,4 @@
-/* The project spec says we need a function or procedure for all of the questions, 
-a cursor for at least 1 of the questions, a trigger for at least 1 question, and a package
-for all of the functions and procedures. 
+/*
 You will submit:
 - A link to your GitHub repository.
 - In your repository, there should be
